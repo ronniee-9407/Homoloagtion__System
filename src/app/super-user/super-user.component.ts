cfbb90115f66676a5a2fd891161e2005c5afa722
ajeevsan <sanjeevmajhi036@gmail.com>
2024-01-17 11:12:25 +0530

    Removed rem in super-user component

--- src/app/super-user/super-user.component.ts
+++ src/app/super-user/super-user.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BackendService } from 'src/services/backend.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import * as echarts from 'echarts';
+
 
 @Component({
   selector: 'app-super-user',
@@ -12,10 +14,68 @@ export class SuperUserComponent implements OnInit {
   constructor(private service: BackendService, private sanitizer: DomSanitizer, private router: Router){}
 
   ngOnInit(): void {
-    
+    this.createChart()
   }
 
+  myChart1: any;
+
+  createChart(){
+    this.myChart1 = echarts.init(document.getElementById('graph_area') as any);
+
+    const option1 = {
+      title: {
+        text: ''
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Current Week', 'Last Week'],
+        textStyle: {
+          color: "white"
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Current Week',
+          type: 'line',
+          smooth: true,
+          stack: 'Total',
+          data: [120, 132, 101,]
+        },
+        {
+          name: 'Last Week',
+          type: 'line',
+          stack: 'Total',
+          smooth: true,
+          data: [220, 182, 191, 234, 290, 330, 310],
+        }
+      ]
+    };
+
+    this.myChart1.setOption(option1);
+  }
+
+
+  //! Function to control the route 
   
-
-
 }
