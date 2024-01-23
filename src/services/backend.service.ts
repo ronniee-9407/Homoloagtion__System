b1c3db5ffea9f977c1f9103be3c3b9f82da875a0
ajeevsan <sanjeevmajhi036@gmail.com>
2024-01-23 15:45:04 +0530

    Added api

--- src/services/backend.service.ts
+++ src/services/backend.service.ts
@@ -10,43 +10,51 @@ export class BackendService {
 
   addCamera(camDetails: any){
     console.log(camDetails);
-    return this.http.post("http://127.0.0.1:9000/add_camera",{"camDetails": camDetails});
+    return this.http.post("http://192.168.68.110:5000/add_camera",{"camDetails": camDetails});
   }
   searchDateTime(query: any, page: any, per_page: any)
   {
     // console.log(query);    
-    return this.http.post("http://127.0.0.1:8000/searchDateTime",{"query": query, "page": page, "per_page": per_page});
+    return this.http.post("http://192.168.68.110:5000/searchDateTime",{"query": query, "page": page, "per_page": per_page});
   }
 
   searchDateTimeFull(query: any)
   {
-    return this.http.post("http://127.0.0.1:8000/searchDateTimeFull",{"query": query});
+    return this.http.post("http://192.168.68.110:5000/searchDateTimeFull",{"query": query});
   }
 
   getDataforInspection(data: any){
     console.log(data);
-    return this.http.post("http://127.0.0.1:8000/vehicle_details",{"data": data});
+    return this.http.post("http://192.168.68.110:5000/vehicle_details",{"data": data});
   }
 
   getVehicleModels(){
-    return this.http.get("http://127.0.0.1:8000/model_list");
+    return this.http.get("http://192.168.68.110:5000/model_list");
   }
   getVehicleVariants(model: any){
-    return this.http.post("http://127.0.0.1:8000/variant_list", {'model': model});
+    return this.http.post("http://192.168.68.110:5000/variant_list", {'model': model});
   }
   getVehicleCc(data: any){
-    return this.http.post("http://127.0.0.1:8000/cc_list", {'data': data});
+    return this.http.post("http://192.168.68.110:5000/cc_list", {'data': data});
   }
 
   getPendingReportDetails(jobId: any){
-    return this.http.post("http://127.0.0.1:8000/report_details", {'jobId': jobId});
+    return this.http.post("http://192.168.68.110:5000/report_details", {'jobId': jobId});
   }
 
   validateUser(data: any){
-    return this.http.post("http://127.0.0.1:8000/validate_user", {'userData': data});
+    return this.http.post("http://192.168.68.110:5000/login", {'userData': data});
   }
 
   adminPasswordReset(data: any){
-    return this.http.post("http://127.0.0.1:8000/reset_password", {'newData': data});
+    return this.http.post("http://192.168.68.110:5000/modify_admin_password", {'newData': data});
+  }
+
+  showNumberOfUsers(){
+    return this.http.get("http://192.168.68.110:5000/number_of_users");
+  }
+
+  showPendingReports(){
+    return this.http.get("http://192.168.68.110:5000/pending_reports");
   }
 }
